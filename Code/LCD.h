9c8c3b3ebe002392534ac5e22e81dc6b587c1bac
Abdelrahman Amr <abdelrahmanamrxo@gmail.com>
2025-05-05 23:35:13 +0300

    Added LCD drivers
    Added LCD.c and LCD.h

---
+++ Code/LCD.h
@@ -0,0 +1,22 @@
+#ifndef LCD_H
+#define LCD_H
+#include <stdbool.h>
+#include "SYSTICK.h"
+#include "tm4c123gh6pm.h"
+#include "GPIO.h"
+
+void printdata(unsigned char data);// Sends a data byte to the LCD module through pins D0-->D7.
+
+void LCD_DATA(unsigned char data);// Sends a data byte to the LCD module.
+
+void LCD_cmd(unsigned char cmd);// Sends a command byte to the LCD module.
+
+void LCD_string(char *str , unsigned char len);// Sends a string to the LCD module.
+
+void LCD_int(void);// Initializes the LCD module.
+
+
+#endif
+
+
+
